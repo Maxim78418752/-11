b12ca1adfed661f48d4e623f857fa230bce0a0db
Максим Семенков <162747915+Maxim78418752@users.noreply.github.com>
2024-03-17 18:05:07 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+my_string = input('Ведите текст: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+first_chart = my_string[0]
+print(first_chart)
+last_chart = my_string[-1]
+print(last_chart)
